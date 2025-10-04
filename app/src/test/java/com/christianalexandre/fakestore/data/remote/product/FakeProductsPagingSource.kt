41aa4ae85294d0b0a68485b7661fa9ed576fb801
Christian Alexandre <christian.alexandre@intelbras.com.br>
2025-10-04 15:10:53 -0300

    test: implement paging test.

--- app/src/test/java/com/christianalexandre/fakestore/data/remote/product/FakeProductsPagingSource.kt
+++ app/src/test/java/com/christianalexandre/fakestore/data/remote/product/FakeProductsPagingSource.kt
@@ -8,14 +8,28 @@ class FakeProductsPagingSource(
     private val products: List<Product>
 ) : PagingSource<Int, Product>() {
     override fun getRefreshKey(state: PagingState<Int, Product>): Int? {
-        return null
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Product> {
+        val currentPage = params.key ?: 0
+        val startIndex = currentPage * params.loadSize
+        val endIndex = (startIndex + 20).coerceAtMost(products.size)
+        val data = products.subList(startIndex, endIndex)
+
+        val nextKey = if (endIndex >= products.size) {
+            null
+        } else {
+            currentPage + 1
+        }
+
         return LoadResult.Page(
-            data = products,
-            prevKey = null,
-            nextKey = null
+            data = data,
+            prevKey = if (currentPage == 0) null else currentPage - 1,
+            nextKey = nextKey
         )
     }
 }
--- app/src/test/java/com/christianalexandre/fakestore/domain/use_case/get_products/GetProductsTest.kt
+++ app/src/test/java/com/christianalexandre/fakestore/domain/use_case/get_products/GetProductsTest.kt
@@ -1,7 +1,10 @@
 package com.christianalexandre.fakestore.domain.use_case.get_products
 
+import androidx.paging.testing.asSnapshot
 import com.christianalexandre.fakestore.data.repository.FakeProductsRepository
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Test
 
 class GetProductsTest {
 
@@ -14,4 +17,13 @@ class GetProductsTest {
         getProducts = GetProducts(fakeProductsRepository)
     }
 
+    @Test
+    fun `getProducts should return a flow of PagingData`() = runTest {
+        val qtyProducts = 100
+        fakeProductsRepository.setProducts(qtyProducts)
+        val products = getProducts().asSnapshot {
+            scrollTo(100)
+        }
+        assert(products.size == qtyProducts)
+    }
 }
