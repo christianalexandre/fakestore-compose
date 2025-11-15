74f059abf32b8243bf1e6dfd047b487f5e54fd9d
Christian Alexandre <christian.alexandre-ext@viavarejo.com.br>
2025-11-15 10:35:56 -0300

    feat: ask for notification post permission.

--- app/src/main/java/com/christianalexandre/fakestore/presentation/feature_screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/christianalexandre/fakestore/presentation/feature_screens/dashboard/DashboardScreen.kt
@@ -1,14 +1,23 @@
 package com.christianalexandre.fakestore.presentation.feature_screens.dashboard
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -18,13 +27,13 @@ import androidx.navigation.navDeepLink
 import com.christianalexandre.fakestore.presentation.feature_screens.account.AccountScreen
 import com.christianalexandre.fakestore.presentation.feature_screens.cart.CartScreen
 import com.christianalexandre.fakestore.presentation.feature_screens.dashboard.components.BottomNavigationBar
+import com.christianalexandre.fakestore.presentation.feature_screens.product_detail.ProductDetailsScreen
+import com.christianalexandre.fakestore.presentation.feature_screens.products_list.ProductsListScreen
 import com.christianalexandre.fakestore.presentation.navigation.graphs.ProductsGraph
 import com.christianalexandre.fakestore.presentation.navigation.routes.AccountScreen
 import com.christianalexandre.fakestore.presentation.navigation.routes.CartScreen
 import com.christianalexandre.fakestore.presentation.navigation.routes.ProductDetailScreen
 import com.christianalexandre.fakestore.presentation.navigation.routes.ProductsScreen
-import com.christianalexandre.fakestore.presentation.feature_screens.product_detail.ProductDetailsScreen
-import com.christianalexandre.fakestore.presentation.feature_screens.products_list.ProductsListScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,6 +44,31 @@ fun DashboardScreen(onLogout: () -> Unit) {
     val currentRoute = navBackStackEntry?.destination?.route
     val shouldShowBottomBar = currentRoute in bottomNavItems.map { it.route }
     val deepLinkUri = "app://fakestore/product_detail/{${ProductDetailScreen.ARG_PRODUCT_ID}}"
+    val context = LocalContext.current
+
+    val permissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = { isGranted ->
+                if (isGranted) {
+                    Log.d("PERMISSIONS", "Granted")
+                } else {
+                    Log.w("PERMISSIONS", "Not granted")
+                }
+            },
+        )
+
+    LaunchedEffect(Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS,
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
 
     Scaffold(
         bottomBar = {
